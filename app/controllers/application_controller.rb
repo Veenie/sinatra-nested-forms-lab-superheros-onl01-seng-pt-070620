526b0103d92d0020228e47adb20636b117070f9a
Raveen K Sharma <raveen08@gmail.com>
2020-10-02 19:31:22 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,9 @@ class App < Sinatra::Base
     @hero1 = params["team"]["members"][0]["name"]
     @hero1pow =  params["team"]["members"][0]["power"]
     @hero1bio = params["team"]["members"][0]["bio"]
-    
+    @hero1 = params["team"]["members"][0]["name"]
+    @hero1pow =  params["team"]["members"][0]["power"]
+    @hero1bio = params["team"]["members"][0]["bio"]
     erb :'team'
   end  
 
